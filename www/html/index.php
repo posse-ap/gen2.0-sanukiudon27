54abb33ae581219fa3cd41e78242c2175174a75b
sanukiudon27 <uudodo0207@icloud.com>
2022-03-03 23:27:55 +0900

    dockerたちあがりました

--- www/html/index.php
+++ www/html/index.php
@@ -1,9 +1,8 @@
 <?php
-$webroot = $_SERVER['DOCUMENT_ROOT'];
-include($webroot."/www/html/connect.php"); 
+// $webroot = $_SERVER['DOCUMENT_ROOT'];
+// include($webroot."/www/html/connect.php"); 
 
 echo "Hello, PHP on Docker!";
 
-echo "good morning!";
 
-phpinfo();
+// phpinfo();
