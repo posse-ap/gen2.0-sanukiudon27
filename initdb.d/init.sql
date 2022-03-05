e7c77a0bc84370fe53aff71b2c0628a928e589fc
sanukiudon27 <uudodo0207@icloud.com>
2022-03-06 02:29:56 +0900

    name_listが取得できない

--- initdb.d/init.sql
+++ initdb.d/init.sql
@@ -1,13 +1,92 @@
 use first;
-DROP TABLE IF EXISTS big_question;
-CREATE TABLE big_question (
+DROP TABLE IF EXISTS big_questions;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS choices;
+CREATE TABLE big_questions (
 id INT NOT NULL AUTO_INCREMENT,
-question_title VARCHAR(225),
+name VARCHAR(225),
 PRIMARY KEY(id)
 );
-drop table if exists test;
-create table test;
-INSERT INTO big_question (question_title) VALUES
+CREATE TABLE questions (
+id INT NOT NULL AUTO_INCREMENT,
+big_question_id INT,
+image VARCHAR(225),
+PRIMARY KEY(id)
+);
+CREATE TABLE choices (
+id INT NOT NULL AUTO_INCREMENT,
+question_id INT,
+name_list VARCHAR(225),
+valid BOOLEAN,
+PRIMARY KEY(id)
+);
+
+
+
+INSERT INTO big_questions (name) VALUES
 ('東京の難読地名クイズ'),
 ('広島の難読地名クイズ');
+
+INSERT INTO questions (big_question_id, image) VALUES
+(1, 'takanawa.png'),
+(1, 'kameido.png'),
+(2, 'mukainada.png');
+
+INSERT INTO choices (question_id, name_list, valid) VALUES
+(1, 'たかなわ', TRUE),
+(1, 'たかわ', FALSE),
+(1, 'こうわ', FALSE),
+(2, 'かめと', FALSE),
+(2, 'かめど', FALSE),
+(2, 'かめいど', TRUE),
+(3, 'むこうひら', FALSE),
+(3, 'むきひら', FALSE),
+(3, 'むかいなだ', TRUE);
+
+-- SELECT 
+-- big_questions.id, big_questions.name, questions.big_question_id, questions.image, choices.question_id, choices.name_list
+-- FROM 
+-- big_questions
+-- INNER JOIN
+-- questions
+-- ON
+-- big_questions.id = questions.big_question_id
+-- INNER JOIN
+-- choices
+-- ON
+-- big_questions.id = choices.question_id
+
+-- CREATE TABLE IF EXISTS all;
+-- CREATE TABLE all ()
+
+DROP TABLE IF EXISTS showed;
+CREATE TABLE 
+showed
+AS
+(SELECT 
+big_questions.name, questions.big_question_id, questions.image, choices.question_id, choices.name_list, big_questions.id
+
+FROM 
+(questions
+JOIN
+choices
+ON
+questions.id = choices.question_id)
+JOIN
+big_questions
+ON
+questions.big_question_id = big_questions.id);
+
+-- DROP TABLE mix;
+-- create table mix as (select choices.id, choices.question_id, choices.name, choices.valid, questions.big_question_id, questions.image from  (choices left outer join questions on choices.question_id = questions.id) left outer join big_questions on questions.big_question_id = big_questions.id);
+
+
+
+
+-- SELECT * FROM
+-- choices left outer join questions on choices.question_id = questions.id left outer join big_questions on questions.big_question_id = big_questions.id
+
+
 -- mysql -u dondon -p first < /docker-entrypoint-initdb.d/init.sql
+-- mysql の更新
+
--- www/html/connect.php
+++ www/html/connect.php
@@ -21,16 +21,42 @@ try {
   ]
   );
   
-  // echo "<p>DB接続に成功しました。</p>";
+  if(isset($_GET['id'])) { $id = $_GET['id']; }
   
+
   // SQL実行
-  $sql = "SELECT * FROM big_question";
+  $sql = "SELECT * FROM big_questions";
   $stmt = $pdo->prepare($sql);
   $stmt->execute();
+
+  // step5
+  // $showed = "SELECT * FROM showed WHERE big_question_id = ?";
+  $stmt2 = $pdo->prepare("SELECT name FROM showed WHERE id = 1");
+  $stmt2->execute();
+  $stmt3 = $pdo->prepare("SELECT name_list FROM showed WHERE id = 1");
+  $stmt3->execute();
+
+  $result2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+  var_dump($result2);
+  $result3 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+  var_dump($result3);
+  
+
+// echo $todo[$id - 1]['name'] . PHP_EOL;
+// echo $todo[$id - 1]['name_list'];
+// echo $todo[$id]['name_list'];
+// echo $todo[$id + 1]['name_list'];
+// echo $todo[$id + 2]['name_list'];
+// echo $todo[$id + 3]['name_list'];
+// echo $todo[$id + 4]['name_list'];
+
   
   // 結果の取得
   $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  var_dump($result);
+  // var_dump($result);
+  // echo $result[0]['name'];
+  // echo $product;
+  // echo $result[$id - 1]['name'];
 } catch (PDOException $e) {
   echo "<p>DB接続エラー</p>";
   echo $e->getMessage();
