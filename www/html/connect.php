2b2f209c2976ea6891aa53fb0b56e44550acb1a3
sanukiudon27 <uudodo0207@icloud.com>
2022-03-10 21:52:37 +0900

    エラーでた原因はわからないが解決、idも付与できた

--- www/html/connect.php
+++ www/html/connect.php
@@ -14,12 +14,12 @@ try {
   // $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
   // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $pdo = new PDO($connect, $user, $pass,
-  // [
-  //   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-  //   PDO::ATTR_EMULATE_PREPARES => false
-  //   // 型変換が自動でされないようにエミュレートモードを変更
-  //   // https://unskilled.site/%E3%81%AA%E3%82%93%E3%81%A7php%E3%81%AFmysql%E3%81%8B%E3%82%89%E3%81%AE%E3%83%AA%E3%82%B6%E3%83%AB%E3%83%88%E3%81%8Cint%E5%9E%8B%E3%81%AE%E3%81%AF%E3%81%9A%E3%81%AA%E3%81%AE%E3%81%ABstring%E5%9E%8B/
-  // ]
+  [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_EMULATE_PREPARES => false
+    // 型変換が自動でされないようにエミュレートモードを変更
+    // https://unskilled.site/%E3%81%AA%E3%82%93%E3%81%A7php%E3%81%AFmysql%E3%81%8B%E3%82%89%E3%81%AE%E3%83%AA%E3%82%B6%E3%83%AB%E3%83%88%E3%81%8Cint%E5%9E%8B%E3%81%AE%E3%81%AF%E3%81%9A%E3%81%AA%E3%81%AE%E3%81%ABstring%E5%9E%8B/
+  ]
 );
 // header('Content-type: image/png');
   
@@ -31,7 +31,7 @@ try {
   // $showed = "SELECT * FROM showed WHERE big_question_id = ?";
   $stmt2 = $pdo->prepare("SELECT name, id FROM showed WHERE id = $id");
   $stmt2->execute();
-  $stmt3 = $pdo->prepare("SELECT DISTINCT id, name_list FROM showed WHERE id = $id");
+  $stmt3 = $pdo->prepare("SELECT id, name_list, valid FROM showed WHERE id = $id");
   $stmt3->execute();
   $stmt4 = $pdo->prepare("SELECT image,question_id FROM showed WHERE id = $id");
   $stmt4->execute();
@@ -43,14 +43,13 @@ try {
   $result4 = $stmt4->fetchAll(PDO::FETCH_ASSOC);
   $result5 = $stmt5->fetchAll(PDO::FETCH_ASSOC);
 
-// var_dump($result5);
+// var_dump($result3);
 // echo $result5[1]['valid'];
 
   // $count = count($result2);
 
 $separate = array_chunk($result3, 3);
-$separate_valid = array_chunk($result5, 3);
-var_dump($separate_valid);
+// $separate_valid = array_chunk($result5, 3);
 // var_dump($separate);
 // $shuffles = shuffle($separate);
 
--- www/html/index.php
+++ www/html/index.php
@@ -30,8 +30,8 @@ require 'connect.php';
         <ul id="selection_container<?= $i; ?>">
           <?php shuffle($separate[$i]); ?>
           <?php for ($u = 0; $u < 3; $u++) : ?>
-            <li id="<?php echo $i ?>-<?php echo $u; ?>">
-            <!-- <?= $separate5[$i][$u]['valid']; ?> -->
+            <li id="<?php echo $i ?>-<?= $separate[$i][$u]['valid']; ?>">
+            <!-- <?= $separate[$i][$u]['valid']; ?> -->
               <?= $separate[$i][$u]['name_list']; ?>
             </li>
           <?php endfor; ?>
@@ -41,37 +41,18 @@ require 'connect.php';
           <p id="what_is_answer<?= $i; ?>" class="whatIsAnswer"></p>
         </div>
       </section>
-      <?php var_dump($separate[$i][$id]['name_list']); ?>
-    <?php endfor; ?>
-  </div>
-
+      <!-- <?php var_dump($separate[$i][$id]['name_list']); ?> -->
   <script>
-    const correct_selection = document.getElementById("<?php echo $i ?>");
+    const correct_selection = document.getElementById(" id="<?php echo $i ?>-<?= $separate[$i][$u]['valid'];" ?>");
     const wrong_selection = document.getElementById(`wrong_selection${i}`);
     const wrong2_selection = document.getElementById(`wrong2_selection${i}`);
     const the_answer = document.getElementById(`the_answer${i}`);
     const what_is_answer = document.getElementById(`what_is_answer${i}`);
 
-
-    // shuffle_choice.forEach(element => {
-    //     let li = document.createElement('li');
-    //     li.innerHTML = element;
-    //     selection_container.appendChild(li);
-    //     if (element === selection_arr[i][0]) {
-    //        li.id = `correct_selection${i}`;
-    //     }
-    //     else if (element === selection_arr[i][1]) {
-    //        li.id = `wrong_selection${i}`;
-    //     }
-    //     else {
-    //        li.id = `wrong2_selection${i}`;
-    //     }
-    //  }
-  </script>
-
-
-
-  <!-- <script src="quizy.js"></script> -->
+  
+    </script>
+      <?php endfor; ?>
+    </div>
 </body>
 
 </html>
