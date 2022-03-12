a9c598382af6fcede1654a49317ab2cdb860d5b1
sanukiudon27 <uudodo0207@icloud.com>
2022-03-12 19:41:45 +0900

    PDO完了

---
+++ www/html/dbconnect.php
@@ -0,0 +1,16 @@
+<?php
+
+$dsn = 'mysql:dbname=webapp;host=db;';
+$user = 'dondon';
+$pass = 'webweb';
+
+try{
+  $pdo = new PDO($dsn, $user, $pass);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+  echo '接続成功っち';
+} catch (PDOException $e){
+$e->getMessage();
+echo '接続失敗だぞん';
+};
+
+?>
