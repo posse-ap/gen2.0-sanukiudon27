c7a2bf71c539d63f90b78bb798572134c647615e
sanukiudon27 <uudodo0207@icloud.com>
2022-03-12 17:18:51 +0900

    クォーテーションを忘れていたことが判明

--- initdb.d/init.sql
+++ initdb.d/init.sql
@@ -1,20 +1,20 @@
 DROP TABLE IF EXISTS sum;
 CREATE TABLE sum (
 id INT AUTO_INCREMENT,
-dayte DATE,
+date DATE,
 time INT,
-contents_id INT,
 lang_id INT,
+contents_id INT,
 PRIMARY KEY(id)
 );
 
 INSERT INTO sum (date, time, lang_id, contents_id) VALUES
-(2020-00-01, 2, 3, 0),
-(2020-01-02, 3, 1, 0),
-(2020-03-12, 5, 2, 1),
-(2020-01-12, 5, 5, 1),
-(2020-02-02, 10, 7, 2),
-(2020-03-17, 4, 8, 2);
+('2020-00-01', 2, 3, 0),
+('2020-01-02', 3, 1, 0),
+('2020-03-12', 5, 2, 1),
+('2020-01-12', 5, 5, 1),
+('2020-02-02', 10, 7, 2),
+('2020-03-17', 4, 8, 2);
 
 
 DROP TABLE IF EXISTS langs;
@@ -25,14 +25,14 @@ PRIMARY KEY(id)
 );
 
 INSERT INTO langs (language) VALUES
-(JavaScript),
-(CSS),
-(PHP),
-(HTML),
-(Laravel),
-(SQL),
-(SHELL),
-(情報システム基礎知識（その他）);
+('JavaScript'),
+('CSS'),
+('PHP'),
+('HTML'),
+('Laravel'),
+('SQL'),
+('SHELL'),
+('情報システム基礎知識（その他）');
 
 
 DROP TABLE IF EXISTS contents;
@@ -43,9 +43,9 @@ PRIMARY KEY(id)
 );
 
 INSERT INTO contents (learn_contents) VALUES
-(ドットインストール),
-(N予備校),
-(POSSE課題);
+('ドットインストール'),
+('N予備校'),
+('POSSE課題');
 
 
 
