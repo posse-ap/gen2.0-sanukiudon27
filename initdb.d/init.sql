3b56ceb0a0b963a4afe701b697240a54049d6f9a
sanukiudon27 <uudodo0207@icloud.com>
2022-03-12 18:40:45 +0900

    テーブル設計完了

--- initdb.d/init.sql
+++ initdb.d/init.sql
@@ -1,8 +1,8 @@
 -- docker-compose up せずにinit.sqlを更新する
 -- mysql -u dondon -p < /docker-entrypoint-initdb.d/init.sql
 use webapp;
-DROP TABLE IF EXISTS sum;
-CREATE TABLE sum (
+DROP TABLE IF EXISTS basic;
+CREATE TABLE basic (
 id INT AUTO_INCREMENT,
 date DATE,
 time INT,
@@ -11,31 +11,31 @@ contents_id INT,
 PRIMARY KEY(id)
 );
 
-INSERT INTO sum (date, time, lang_id, contents_id) VALUES
-('2020-02-01', 2, 3, 0),
-('2020-01-02', 3, 1, 0),
-('2020-03-12', 5, 2, 1),
-('2020-01-12', 5, 5, 1),
-('2020-02-02', 10, 7, 2),
-('2020-03-17', 4, 8, 2);
+INSERT INTO basic (date, time, lang_id, contents_id) VALUES
+('2020-02-01', 2, 3, 1),
+('2020-01-02', 3, 1, 1),
+('2020-03-12', 5, 2, 2),
+('2020-01-12', 5, 5, 2),
+('2020-02-02', 10, 7, 3),
+('2020-03-17', 4, 8, 3);
 
 -- +----+------------+------+---------+-------------+
 -- | id | date       | time | lang_id | contents_id |
 -- +----+------------+------+---------+-------------+
--- |  1 | 2020-02-01 |    2 |       3 |           0 |
--- |  2 | 2020-01-02 |    3 |       1 |           0 |
--- |  3 | 2020-03-12 |    5 |       2 |           1 |
--- |  4 | 2020-01-12 |    5 |       5 |           1 |
--- |  5 | 2020-02-02 |   10 |       7 |           2 |
--- |  6 | 2020-03-17 |    4 |       8 |           2 |
+-- |  1 | 2020-02-01 |    2 |       3 |           1 |
+-- |  2 | 2020-01-02 |    3 |       1 |           1 |
+-- |  3 | 2020-03-12 |    5 |       2 |           2 |
+-- |  4 | 2020-01-12 |    5 |       5 |           2 |
+-- |  5 | 2020-02-02 |   10 |       7 |           3 |
+-- |  6 | 2020-03-17 |    4 |       8 |           3 |
 -- +----+------------+------+---------+-------------+
 
 
 DROP TABLE IF EXISTS langs;
 CREATE TABLE langs (
-id INT AUTO_INCREMENT,
+langs_table_id INT AUTO_INCREMENT,
 language VARCHAR(255),
-PRIMARY KEY(id)
+PRIMARY KEY(langs_table_id)
 );
 
 INSERT INTO langs (language) VALUES
@@ -64,9 +64,9 @@ INSERT INTO langs (language) VALUES
 
 DROP TABLE IF EXISTS contents;
 CREATE TABLE contents (
-id INT AUTO_INCREMENT,
+contents_table_id INT AUTO_INCREMENT,
 learn_contents VARCHAR(255),
-PRIMARY KEY(id)
+PRIMARY KEY(contents_table_id)
 );
 
 INSERT INTO contents (learn_contents) VALUES
@@ -82,8 +82,25 @@ INSERT INTO contents (learn_contents) VALUES
 -- |  3 | POSSE課題                   |
 -- +----+-----------------------------+
 
+DROP TABLE IF EXISTS sum;
+CREATE TABLE sum (
+SELECT * FROM basic
+LEFT JOIN langs
+ON basic.lang_id = langs.langs_table_id
+LEFT JOIN contents 
+ON basic.contents_id = contents.contents_table_id
+);
 
-
+-- +----+------------+------+---------+-------------+----------------+-----------------------------------------------+-------------------+-----------------------------+
+-- | id | date       | time | lang_id | contents_id | langs_table_id | language                                      | contents_table_id | learn_contents              |
+-- +----+------------+------+---------+-------------+----------------+-----------------------------------------------+-------------------+-----------------------------+
+-- |  1 | 2020-02-01 |    2 |       3 |           1 |              3 | PHP                                           |                 1 | ドットインストール |
+-- |  2 | 2020-01-02 |    3 |       1 |           1 |              1 | JavaScript                                    |                 1 | ドットインストール |
+-- |  3 | 2020-03-12 |    5 |       2 |           2 |              2 | CSS                                           |                 2 | N予備校                  |
+-- |  4 | 2020-01-12 |    5 |       5 |           2 |              5 | Laravel                                       |                 2 | N予備校                  |
+-- |  5 | 2020-02-02 |   10 |       7 |           3 |              7 | SHELL                                         |                 3 | POSSE課題                 |
+-- |  6 | 2020-03-17 |    4 |       8 |           3 |              8 | 情報システム基礎知識（その他） |                 3 | POSSE課題                 |
+-- +----+------------+------+---------+-------------+----------------+-----------------------------------------------+-------------------+-----------------------------+
 
 
 
