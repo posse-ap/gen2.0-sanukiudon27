58148b1f99804e56e2a5f95ef66a9703e6fc84c0
sanukiudon27 <uudodo0207@icloud.com>
2022-03-12 14:40:49 +0900

    テーブル作成中

---
+++ initdb.d/init.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS sum;
+CREATE TABLE sum (
+id INT AUTO_INCREMENT,
+day DATE,
+time INT,
+contents VARCHAR(255),
+lang VARCHAR(255),
+PRIMARY KEY(id)
+
+);
+
+INSERT INTO sum (day, time, contents, lang) VALUES
+(2020),
+(),
+();
