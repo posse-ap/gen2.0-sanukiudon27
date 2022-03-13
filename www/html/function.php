b833da69c529c0a78237604afc643d2d08b09ab5
sanukiudon27 <uudodo0207@icloud.com>
2022-03-13 11:52:17 +0900

    チャート表示でつまってる

--- www/html/function.php
+++ www/html/function.php
@@ -40,3 +40,57 @@ function sum_day($pdo)
   }
   return $sum;
 }
+
+
+$stmt = $pdo->prepare("SELECT DATE_FORMAT(date, '%Y-%m-%d') AS day, time from sum");
+$stmt->execute();
+$day = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+// var_dump(date("Y-m-d"));であればstring(10) "2022-03-03"のように表示されるが、for文で回すので日付が3だけと一桁になってしまうので、二けたにする関数
+// for ($d=1; $d <= date('t') ; $d++) { 
+//   # code...
+// }
+$array = [];
+
+for ($i = 1; $i <= date('t'); $i++) {
+  $padding[$i] = (str_pad(date("$i"), 2, 0, STR_PAD_LEFT));
+  // var_dump(date("Y-m-$padding[$i]"));
+  for ($u=0; $u < count($day) ; $u++) { 
+    if(date("Y-m-$padding[$i]") === $day[$u]['day']){
+    $day[$u]['time'];
+    array_push($array,$day[$u]['time']);
+  }
+}
+}
+var_dump($array);
+
+// var_dump($day[1]['day']);
+// for ($i=1; $i <= date('t'); $i++) { 
+//   var_dump(date("Y-m-$i"));
+// }
+
+// for($u=0; $u< count($day); $u++){
+//   for($i=1; $i<=date('t'); $i++) {
+//     if($day[$u]['day'] === date("Y-m-$i")){
+//       var_dump(explode('-', $day[$u]['day']));
+//       // var_dump((INT)$day[$u]['day']);
+//     }
+//   }
+  
+  // var_dump($day[$u]['day']);
+  // var_dump($day);
+  // var_dump(count($day));
+  // var_dump(date("Y-m-$i"));
+// }
+// for($i=1; $i<=date('t'); $i++) {
+//   $stmtZero[$i] = date('Y-m-d', strtotime($day[$i]['day']));
+//   var_dump($stmtZero);
+// // var_dump(date("Y-m-$i"));
+// }
+// var_dump($day[0]['day']);
+
+// var_dump(date("Y-m-1"));
+// var_dump($day[0]['day']);
+
+// var_dump(date("Y-m-1") == $day[0]['day']);
